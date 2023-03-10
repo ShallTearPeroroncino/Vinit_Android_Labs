10bfc434ad125dd37aec22a5807cf1b7864404c5
Vinit Somaiya <soma0036@algonquinlive.com>
2023-03-10 13:09:22 -0500

    Week5 Finally Finished

--- app/src/androidTest/java/algonquin/cst2335/soma0036/MainActivityTest.java
+++ app/src/androidTest/java/algonquin/cst2335/soma0036/MainActivityTest.java
@@ -37,194 +37,62 @@ public class MainActivityTest {
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest() {
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void testfindMissingUpperCase() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editPass));
+        appCompatEditText.perform(replaceText("vinit@123"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.editPass),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText.perform(replaceText("ccc"), closeSoftKeyboard());
-
-
-
-
-        ViewInteraction materialButton = onView(
-                allOf(withId(R.id.login), withText("Login"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
+        ViewInteraction materialButton = onView(allOf(withId(R.id.login)));
         materialButton.perform(click());
 
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.passText), withText("You shall not pass"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
+        ViewInteraction textView = onView((withId(R.id.passText)));
         textView.check(matches(withText("You shall not pass")));
+    }
 
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.passText), withText("You shall not pass"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView2.check(matches(withText("You shall not pass")));
+    @Test
+    public void testfindMissingLowerCase() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editPass));
+        appCompatEditText.perform(replaceText("VINIT@123"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.editPass), withText("ccc"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText2.perform(replaceText("AAA"));
+        ViewInteraction materialButton = onView(allOf(withId(R.id.login)));
+        materialButton.perform(click());
 
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.editPass), withText("AAA"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText3.perform(closeSoftKeyboard());
+        ViewInteraction textView = onView((withId(R.id.passText)));
+        textView.check(matches(withText("You shall not pass")));
+    }
 
-        ViewInteraction materialButton2 = onView(
-                allOf(withId(R.id.login), withText("Login"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        materialButton2.perform(click());
+    @Test
+    public void testfindMissingNumber() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editPass));
+        appCompatEditText.perform(replaceText("Vinit@"), closeSoftKeyboard());
 
-        ViewInteraction textView3 = onView(
-                allOf(withId(R.id.passText), withText("You shall not pass"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView3.check(matches(withText("You shall not pass")));
+        ViewInteraction materialButton = onView(allOf(withId(R.id.login)));
+        materialButton.perform(click());
 
-        ViewInteraction appCompatEditText4 = onView(
-                allOf(withId(R.id.editPass), withText("AAA"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText4.perform(replaceText("AAAccc"));
+        ViewInteraction textView = onView((withId(R.id.passText)));
+        textView.check(matches(withText("You shall not pass")));
+    }
+    @Test
+    public void testfindMissingSpecialCharacter() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editPass));
+        appCompatEditText.perform(replaceText("Vinit123"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText5 = onView(
-                allOf(withId(R.id.editPass), withText("AAAccc"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText5.perform(closeSoftKeyboard());
+        ViewInteraction materialButton = onView(allOf(withId(R.id.login)));
+        materialButton.perform(click());
 
-        ViewInteraction materialButton3 = onView(
-                allOf(withId(R.id.login), withText("Login"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        materialButton3.perform(click());
+        ViewInteraction textView = onView((withId(R.id.passText)));
+        textView.check(matches(withText("You shall not pass")));
+    }
 
-        ViewInteraction textView4 = onView(
-                allOf(withId(R.id.passText), withText("You shall not pass"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView4.check(matches(withText("You shall not pass")));
+    @Test
+    public void testAllRequirements() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editPass));
+        appCompatEditText.perform(replaceText("Vinit@123"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText6 = onView(
-                allOf(withId(R.id.editPass), withText("AAAccc"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText6.perform(replaceText("AAAccc123"));
+        ViewInteraction materialButton = onView(allOf(withId(R.id.login)));
+        materialButton.perform(click());
 
-        ViewInteraction appCompatEditText7 = onView(
-                allOf(withId(R.id.editPass), withText("AAAccc123"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText7.perform(closeSoftKeyboard());
-
-        ViewInteraction materialButton4 = onView(
-                allOf(withId(R.id.login), withText("Login"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        materialButton4.perform(click());
-
-        ViewInteraction textView5 = onView(
-                allOf(withId(R.id.passText), withText("You shall not pass"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView5.check(matches(withText("You shall not pass")));
-
-        ViewInteraction appCompatEditText8 = onView(
-                allOf(withId(R.id.editPass), withText("AAAccc123"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText8.perform(replaceText("AAAccc123$$"));
-
-        ViewInteraction appCompatEditText9 = onView(
-                allOf(withId(R.id.editPass), withText("AAAccc123$$"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                2),
-                        isDisplayed()));
-        appCompatEditText9.perform(closeSoftKeyboard());
-
-        ViewInteraction materialButton5 = onView(
-                allOf(withId(R.id.login), withText("Login"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        materialButton5.perform(click());
-
-        ViewInteraction textView6 = onView(
-                allOf(withId(R.id.passText), withText("Your password meets the requirements"),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()));
-        textView6.check(matches(withText("Your password meets the requirements")));
+        ViewInteraction textView = onView((withId(R.id.passText)));
+        textView.check(matches(withText("Your password meets the requirements")));
     }
 
     private static Matcher<View> childAtPosition(
@@ -236,7 +104,6 @@ public class MainActivityTest {
                 description.appendText("Child at position " + position + " in parent ");
                 parentMatcher.describeTo(description);
             }
-
             @Override
             public boolean matchesSafely(View view) {
                 ViewParent parent = view.getParent();
